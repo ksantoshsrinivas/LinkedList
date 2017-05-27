663db5b862b4c78e86c4bd70068f9e13f2709cb2
ksantoshsrinivas <ksantoshsrinivas@gmail.com>
2017-05-27 19:17:34 +0530

    Deleted display function

--- insert.c
+++ insert.c
@@ -76,17 +76,3 @@ void atEnd(struct node **head_ref, int n)
    last->next = p;
    return;
 }
-
-/*void display(struct node **head_ref)
-{
-   struct node *p = NULL;
-   if(*head_ref==NULL)
-   {
-      printf("List is empty\n");
-      return;
-   }
-   printf("The elements in the list are:\n");
-   for(p=*head_ref;p->next!=NULL;p=p->next)
-      printf("%d->", p->data);
-   printf("%d\n", p->data);
-}*/
